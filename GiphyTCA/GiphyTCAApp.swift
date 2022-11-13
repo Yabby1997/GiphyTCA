1f04d27db5f9ae6d6ebf7564a053fe40ad7911c3
Yabby1997 <yabby1997@gmail.com>
2022-11-13 18:11:41 +0900

    Initial Commit

---
+++ GiphyTCA/GiphyTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyTCAApp.swift
+//  GiphyTCA
+//
+//  Created by USER on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct GiphyTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
